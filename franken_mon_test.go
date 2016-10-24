b6b0c40cc6959a388d032e89f203197c491931d7
Chris Townsend <ctownsen357@gmail.com>
2016-10-24 13:54:10 -0400

    Cleaned up the testing.

--- franken_mon_test.go
+++ franken_mon_test.go
@@ -1,22 +1,28 @@
 package main
 
 import (
+	"log"
+	"reflect"
 	"strings"
 	"testing"
 
 	"github.com/fsouza/go-dockerclient"
 )
 
+func expect(t *testing.T, a interface{}, b interface{}) {
+	if a != b {
+		t.Errorf("Expected %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+	}
+}
+
 func TestGetConfig(t *testing.T) {
 	config, err := GetConfig()
 	if err != nil {
-		t.Fatal("Error was not nil:", err)
+		log.Fatal(err)
 	}
 
 	expected := "restart"
-	if config.ActionToMonitor != expected {
-		t.Errorf("Configuration file is not returning expected value.  Expected %s but received %s ", expected, config.ActionToMonitor)
-	}
+	expect(t, config.ActionToMonitor, expected)
 }
 
 func TestParseCommandTemplate(t *testing.T) {
@@ -40,24 +46,22 @@ func TestParseCommandTemplate(t *testing.T) {
 
 	rslt, err := ParseCommandTemplate(cmdTemplate, events[0])
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
 	expected := "docker exec 5745704abe9caa5 pwd"
-	if rslt != expected {
-		t.Errorf("The command did not parse correctly.  Expected %v but got %v", expected, rslt)
-	}
+	expect(t, rslt, expected)
 }
 
 func TestExecuteCommand(t *testing.T) {
 	rslt, err := ExecuteCommand("echo 'testing 1,2,3...'")
 	if err != nil {
-		t.Fatal("Error trying to execute command:", err)
+		log.Fatal("Error trying to execute command:", err)
 	}
 
 	expected := "testing 1,2,3..."
 
 	if !strings.Contains(rslt, expected) {
-		t.Errorf("The command did not run correctly.  Expected %v but got %v", expected, rslt)
+		t.Errorf("Expected %v (type %v) - Got %v (type %v)", expected, reflect.TypeOf(expected), rslt, reflect.TypeOf(rslt))
 	}
 
 }
